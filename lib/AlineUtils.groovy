1c8f4c24eb594c5984f8b303d0a7b8838876c440
Jacques Dainat <jacques.dainat@ird.fr>
2025-06-10 17:45:32 +0200

    add general functions in lib

---
+++ lib/AlineUtils.groovy
@@ -0,0 +1,29 @@
+// dedicated to the utility functions
+class AlineUtils {
+
+    // Function to chek if we
+    public static Boolean is_url(file) {
+        
+        // check if the file is a URL
+        if (file =~ /^(http|https|ftp|s3|az|gs):\/\/.*/) {
+            return true
+        } else {
+            return false
+        }
+    }
+    // Function to chek if we
+    public static Boolean is_fastq(file) {
+        
+        // check if the file is a URL
+        if (file =~ /.*(fq|fastq|fq.gz|fastq.gz)$/) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    // Function to extract the basename of a file
+    public static String getCleanName(file) {
+        return file[0].baseName.replaceAll(/\.(fastq|fq)$/, '')
+    }
+}
